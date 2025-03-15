7130869ba23ec803193cdf0d362da2de3fc3b2a2
TRAN QUANG DUNG <tqd0105@gmail.com>
2025-03-15 11:17:51 +0700

    Update Post.jsx

--- src/components/main/Post.jsx
+++ src/components/main/Post.jsx
@@ -2,25 +2,10 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function Post () {
-    const [posts, setPosts] = useState([]);
 
-    useEffect(()=> {
-        axios.get("https://spacezone-api.onrender.com/posts", { cache: "no-store" }) // Đảm bảo không dùng cache
-        .then((response)=>{
-            setPosts(response.data);
-        })
-        .catch((error)=>{
-            console.log(error);
-        })
-    },[])
-    
     return (
         <ul>
-            {posts.map((post)=>(
-                <div>
-                <li key={post.id}>{post.name} </li>
-                <img src={post.image} alt="" /></div>
-            ))}
+            
         </ul>
     )
 }
