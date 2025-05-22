a685a6b07de3b51f6ba3a07911dab60761ae0a33
TRAN QUANG DUNG <tqd0105@gmail.com>
2025-05-22 20:29:06 +0700

    fix hover Profile

--- src/components/main/PrivacySelector.jsx
+++ src/components/main/PrivacySelector.jsx
@@ -35,7 +35,7 @@ function PrivacySelector() {
     <div className="relative  flex-column-center w-[130px]" ref={dropdownRef}>
       <button
         onClick={() => setShowOptions(!showOptions)}
-        className="flex-row-center gap-1 w-full rounded-md border border-gray-400 p-2 hover:bg-gray-900 text-sm font-semibold"
+        className="flex-row-center gap-1 w-full rounded-md border border-gray-400 p-2 hover:bg-gray-200 text-sm font-semibold"
       >
         <img src={selectedOption?.icon} width={16} height={16} alt="" />
         <span style={{ color: "black" }}>
